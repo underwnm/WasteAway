b3efebd972e602a1513a4c910ebde225f8a2d4b8
underwnm <underwnm@gmail.com>
2016-11-28 14:41:29 -0600

    Add VacationViewmodel.

---
+++ WasteAway/ViewModels/VacationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using WasteAway.Models;
+
+namespace WasteAway.ViewModels
+{
+    public class VacationViewModel
+    {
+        [Required]
+        public int DayId { get; set; }
+        public Day Day { get; set; }
+
+        [Required]
+        public int MonthId { get; set; }
+        public Month Month { get; set; }
+
+        [Required]
+        public int YearId { get; set; }
+        public Year Year { get; set; }
+
+        public DateTime Test()
+        {
+            var leaveDate = new DateTime(DayId, MonthId, YearId);
+
+            return leaveDate;
+        }
+    }
+}
