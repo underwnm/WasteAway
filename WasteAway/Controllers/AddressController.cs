b6a1cc669b2e97215cd75fe2cac1ee9a630e0025
underwnm <underwnm@gmail.com>
2016-11-27 18:22:59 -0600

    Create add a pickup address form.

---
+++ WasteAway/Controllers/AddressController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using WasteAway.Models;
+using WasteAway.ViewModels;
+
+namespace WasteAway.Controllers
+{
+    public class AddressController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AddressController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new AddressFormViewModel
+            {
+                States = _context.States.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ WasteAway/ViewModels/AddressFormViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using WasteAway.Models;
+
+namespace WasteAway.ViewModels
+{
+    public class AddressFormViewModel
+    {
+        [Required]
+        [Display(Name = "Street Address Line 1")]
+        public string StreetAddressOne { get; set; }
+
+        [Display(Name = "Street Address Line 2")]
+        public string StreetAddressTwo { get; set; }
+
+        [Required]
+        [Display(Name = "State")]
+        public int StateId { get; set; }
+        public IEnumerable<State> States { get; set; }
+
+        [Required]
+        [Display(Name = "City")]
+        public int? CityId { get; set; }
+
+        [Required]
+        [Display(Name = "Zipcode")]
+        public int? ZipcodeId { get; set; }
+    }
+}
