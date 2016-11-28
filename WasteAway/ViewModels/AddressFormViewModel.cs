35188b7493fa402cbbaead77703b8e6377c5d263
underwnm <underwnm@gmail.com>
2016-11-27 20:32:06 -0600

    Improve the look and feel.

--- WasteAway/ViewModels/AddressFormViewModel.cs
+++ WasteAway/ViewModels/AddressFormViewModel.cs
@@ -7,27 +7,27 @@ namespace WasteAway.ViewModels
     public class AddressFormViewModel
     {
         [Required]
-        [Display(Name = "Street Address Line")]
+        [Display(Name = "Street Address Line*")]
         public string StreetAddressOne { get; set; }
 
         [Display(Name = "Street Address Line Cont.")]
         public string StreetAddressTwo { get; set; }
 
         [Required]
-        [Display(Name = "State")]
+        [Display(Name = "State*")]
         public int StateId { get; set; }
         public IEnumerable<State> States { get; set; }
 
         [Required]
-        [Display(Name = "City")]
+        [Display(Name = "City*")]
         public string City { get; set; }
 
         [Required]
-        [Display(Name = "Zipcode")]
+        [Display(Name = "Zipcode*")]
         public string ZipcodeId { get; set; }
 
         [Required]
-        [Display(Name = "Weekly Pickup Day")]
+        [Display(Name = "Weekly Pickup Day*")]
         public int WeekdayId { get; set; }
         public IEnumerable<Weekday> Weekdays { get; set; }
     }
