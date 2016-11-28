b779471e2d9fe907de868ab769e18c8f54e0fba4
underwnm <underwnm@gmail.com>
2016-11-28 15:15:00 -0600

    Populate Years table.

---
+++ WasteAway/Migrations/201611282113022_PopulateYearsTable.cs
@@ -0,0 +1,20 @@
+namespace WasteAway.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class PopulateYearsTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("SET IDENTITY_INSERT Years ON");
+            Sql("INSERT INTO Years (Id, Name) VALUES (1, '2016')");
+            Sql("INSERT INTO Years (Id, Name) VALUES (2, '2017')");
+            Sql("SET IDENTITY_INSERT Years OFF");
+        }
+        
+        public override void Down()
+        {
+            Sql("DELETE FROM Months WHERE Id IN (1, 2");
+        }
+    }
+}
