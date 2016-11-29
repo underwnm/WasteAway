e6e07fd83055db7741d8feea4a42abe76aa56c20
underwnm <underwnm@gmail.com>
2016-11-29 12:20:07 -0600

    Fix change address to use dropdown list for cities and zipcodes.

--- WasteAway/ViewModels/ChangeAddressViewModel.cs
+++ WasteAway/ViewModels/ChangeAddressViewModel.cs
@@ -17,39 +17,30 @@ namespace WasteAway.ViewModels
         [Required]
         [Display(Name = "State*")]
         public int StateId { get; set; }
+
         public IEnumerable<State> States { get; set; }
 
         [Required]
         [Display(Name = "City*")]
-        public string City { get; set; }
+        public int CityId { get; set; }
+
+        public IEnumerable<City> Cities { get; set; }
 
         [Required]
         [Display(Name = "Zipcode*")]
-        public string ZipcodeId { get; set; }
+        public int ZipcodeId { get; set; }
+
+        public IEnumerable<Zipcode> Zipcodes { get; set; }
 
         public int ChangeAddress(ApplicationDbContext context)
         {
-            var city = new City
-            {
-                StateId = StateId,
-                Name = City
-            };
-            context.Cities.Add(city);
-            context.SaveChanges();
-
-            var zipcode = new Zipcode
-            {
-                Name = ZipcodeId,
-            };
-            context.Zipcodes.Add(zipcode);
-            context.SaveChanges();
 
             var address = new Address
             {
                 StreetAddressOne = StreetAddressOne,
                 StreetAddressTwo = StreetAddressTwo,
-                CityId = city.Id,
-                ZipcodeId = zipcode.Id
+                CityId = CityId,
+                ZipcodeId = ZipcodeId
             };
             context.Addresses.Add(address);
             context.SaveChanges();
