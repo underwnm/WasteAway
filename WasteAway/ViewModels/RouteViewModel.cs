ed78ec64c971063550c10db5b50f4a1af9deef33
underwnm <underwnm@gmail.com>
2016-11-29 18:01:39 -0600

    Refactor queries into entity framework lambda expressions

--- WasteAway/ViewModels/RouteViewModel.cs
+++ WasteAway/ViewModels/RouteViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Data.SqlClient;
 using System.Linq;
 using WasteAway.Models;
 
@@ -30,35 +29,33 @@ namespace WasteAway.ViewModels
 
         private List<ApplicationUser> CreatePickupList(bool suspendPickup)
         {
-            var pickupList = new List<ApplicationUser>();
+            var pickups = new List<ApplicationUser>();
             var date = new DateTime();
             var currentDay = date.DayOfWeek.ToString();
 
-            var query = (from a in _context.Users
-                         where a.AlternatePickupWeekday.Name 
-                            == currentDay && !suspendPickup
-                         select a);
+            var results = _context.Users
+                .Where(a => a.AlternatePickupWeekday.Name == currentDay && !suspendPickup)
+                .ToList();
 
-            foreach (var user in query)
+            foreach (var user in results)
             {
-                pickupList.Add(user);
+                pickups.Add(user);
                 user.AlternatePickupWeekdayId = null;
                 user.Balance += PickupCost;
             }
 
-            query = (from a in _context.Users
-                     where a.PickupWeekday.Name 
-                        == currentDay && a.AlternatePickupWeekdayId 
-                        == null && !suspendPickup
-                     select a);
+            results = _context.Users
+                    .Where(a => a.PickupWeekday.Name == currentDay)
+                    .Where(a => a.AlternatePickupWeekdayId == null && !suspendPickup)
+                    .ToList();
 
-            foreach (var user in query)
+            foreach (var user in results)
             {
-                pickupList.Add(user);
+                pickups.Add(user);
                 user.Balance += PickupCost;
             }
 
-            return pickupList;
+            return pickups;
         }
 
         private void SetTruckPickups(List<ApplicationUser> users)
@@ -89,13 +86,6 @@ namespace WasteAway.ViewModels
             {
                 truck.Pickups = new List<Pickup>();
             }
-        }
-        
-        private static void ClearPickups()
-        {
-            const string truncate = "TRUNCATE TABLE Pickups";
-            var command = new SqlCommand(truncate);
-            command.ExecuteNonQuery();
         }
     }
 }
