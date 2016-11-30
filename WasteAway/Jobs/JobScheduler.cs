1977ea25b83834b388a5b33440ac71da141029b8
underwnm <underwnm@gmail.com>
2016-11-30 13:32:12 -0600

    Fix global job for building route each day

---
+++ WasteAway/Jobs/JobScheduler.cs
@@ -0,0 +1,27 @@
+﻿using Quartz;
+using Quartz.Impl;
+
+namespace WasteAway.Jobs
+{
+    public class JobScheduler
+    {
+        public static void Start()
+        {
+            var scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+
+            var job = JobBuilder.Create<RouteJob>().Build();
+
+            var trigger = TriggerBuilder.Create()
+                .WithDailyTimeIntervalSchedule
+                (s =>
+                    s.WithIntervalInHours(24)
+                    .OnEveryDay()
+                    .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(8, 0))
+                )
+                .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
